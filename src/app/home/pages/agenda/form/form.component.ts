1bb75d52a169a5d37c3de05801b521438eb6b6f0
Alex Tigselema <kaiser.alex1994@gmail.com>
2023-06-07 15:15:12 -0500

    Servicios de agenda

---
+++ src/app/home/pages/agenda/form/form.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { Agenda } from 'src/app/shared/models/agenda.model';
+import { FormService } from '../services/form.service';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss'],
+})
+export class FormComponent implements OnInit {
+  visible: boolean = false;
+  agendaItem!: Agenda;
+  constructor(private formService: FormService) {}
+  ngOnInit(): void {
+    this.formService.open$.subscribe({
+      next: (resp) => {
+        this.visible = resp.open;
+      },
+    });
+  }
+
+  onClose(): void {
+    this.formService.onOpen({ open: false, data: this.agendaItem });
+  }
+}
