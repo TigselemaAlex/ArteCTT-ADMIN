b99553347b1aab02ca97b3f5f46b2f62740d67f7
Alex Tigselema <kaiser.alex1994@gmail.com>
2023-07-12 11:42:19 -0500

    Se añadieron unos cambios finales de estilos

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -5,9 +5,11 @@ import { LoginRoutingModule } from './login-routing.module';
 import { MainComponent } from './pages/main/main.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { SharedModule } from '../shared/shared.module';
+import { MessageService } from 'primeng/api';
 
 @NgModule({
   declarations: [MainComponent, LoginFormComponent],
   imports: [CommonModule, LoginRoutingModule, SharedModule],
+  providers: [MessageService],
 })
 export class LoginModule {}
